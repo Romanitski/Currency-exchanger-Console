45eed865efc4aa001ad07833b4fe4349f6efdf54
Romanitski <vromanitski@gmail.com>
2023-07-13 15:21:50 +0300

    Added model classes.
    The class of currencies and coefficients.

---
+++ CurrencyExchangerConsole/CurrencyExchangerConsole/Classes/Coefficients.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CurrencyExchangerConsole.Classes
+{
+    public class Coefficients
+    {
+        private static Coefficients instance;
+
+        public static Coefficients GetInstance()
+        {
+            return instance ?? (instance = new Coefficients());
+        }
+
+        public Coefficients() { }
+
+        public Coefficients(string coefficient, DateTime dateOfIssue, DateTime dateOfTheStartAction)
+        {
+            this.coefficient = coefficient;
+            this.dateOfIssue = dateOfIssue;
+            this.dateOfTheStartAction = dateOfTheStartAction;
+        }
+
+        public string coefficient { get; set; }
+        public DateTime dateOfIssue { get; set; }
+        public DateTime dateOfTheStartAction { get; set; }
+    }
+}
---
+++ CurrencyExchangerConsole/CurrencyExchangerConsole/Classes/Currencies.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CurrencyExchangerConsole.Classes
+{
+    public class Currencies
+    {
+        private static Currencies instance;
+
+        public static Currencies GetInstance()
+        {
+            return instance ?? (instance = new Currencies());
+        }
+
+        public Currencies () { }
+
+        public Currencies(string currencyName, string alphabeticCurrencyCode, string numberOfCurrencyUnits)
+        {
+            this.currencyName = currencyName;
+            this.alphabeticCurrencyCode = alphabeticCurrencyCode;
+            this.numberOfCurrencyUnits = numberOfCurrencyUnits;
+        }
+
+        public Currencies(int digitalCurrencyCode, string currencyName, string alphabeticCurrencyCode, string numberOfCurrencyUnits)
+        {
+            this.digitalCurrencyCode = digitalCurrencyCode;
+            this.currencyName = currencyName;
+            this.alphabeticCurrencyCode = alphabeticCurrencyCode;
+            this.numberOfCurrencyUnits = numberOfCurrencyUnits;
+        }
+
+        public int digitalCurrencyCode { get; set; }
+        public string alphabeticCurrencyCode { get; set; }
+        public string currencyName { get; set; }
+        public string numberOfCurrencyUnits { get; set; }
+    }
+}
