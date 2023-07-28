13ed69cbb35205021975bb74900c289d8d7451b4
Romanitski <vromanitski@gmail.com>
2023-07-28 07:03:11 +0300

    Added SQLite connection.

---
+++ CurrencyExchangerConsole/CurrencyExchangerConsole/Classes/SQLite.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace CurrencyExchangerConsole.Classes
+{
+    class SQLite
+    {
+        public void connection()
+        {
+            using (var connection = new SqliteConnection("Data Source=currencyExchanger.db"))
+            {
+                connection.Open();
+            }
+        }
+    }
+}
